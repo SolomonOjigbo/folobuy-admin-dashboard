8c26b18c4d82b318daa5fd4e3550ea0e23dbbd58
SolomonOjigbo <solomonojigbo@gmail.com>
2022-10-10 17:11:31 +0100

    Outlined the project folder structure and configured react-router-dom to manage the navigation to pages and paths

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import List from "./pages/List/List";
+import Login from "./pages/Login/Login";
+import New from "./pages/New/New";
+import SinglePage from "./pages/SinglePage/SinglePage";
 
 function App() {
 	return (
-		<div className="App">
-			<div className="AppGlass">App</div>
+		<div>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/">
+						<Route index element={<Home />} />
+						<Route path="login" element={<Login />} />
+						<Route path="users">
+							<Route index element={<List />} />
+							<Route path=":userId" element={<SinglePage />} />
+							<Route path="new" element={<New title="Add New User" />} />
+						</Route>
+						<Route path="products">
+							<Route index element={<List />} />
+							<Route path=":productId" element={<SinglePage />} />
+							<Route path="new" element={<New title="Add New Product" />} />
+						</Route>
+					</Route>
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
