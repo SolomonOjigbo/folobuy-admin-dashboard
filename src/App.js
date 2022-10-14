cd230b0b4bd9c772840778f72988fb1ec4528f65
SolomonOjigbo <solomonojigbo@gmail.com>
2022-10-14 12:42:53 +0100

    Created the DarkModeContext to switch app appearance between Light and Dark mode

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import { useContext } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { DarkModeContext } from "./context/DarkModeContext";
 import { productInputs, userInputs } from "./data/formSource";
 import Home from "./pages/Home/Home";
 import List from "./pages/List/List";
 import Login from "./pages/Login/Login";
 import New from "./pages/New/New";
 import SinglePage from "./pages/SinglePage/SinglePage";
+import "./styles/Dark.scss";
 
 function App() {
+	const { darkMode } = useContext(DarkModeContext);
 	return (
-		<div>
+		<div className={darkMode ? "app dark" : "app"}>
 			<BrowserRouter>
 				<Routes>
 					<Route path="/">
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Navbar.scss";
 import ProfileImg from "../../assests/profile1.jpg";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
@@ -8,8 +8,10 @@ import FullscreenExitOutlinedIcon from "@mui/icons-material/FullscreenExitOutlin
 import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
 import ChatBubbleOutlineOutlinedIcon from "@mui/icons-material/ChatBubbleOutlineOutlined";
 import ListOutlinedIcon from "@mui/icons-material/ListOutlined";
+import { DarkModeContext } from "../../context/DarkModeContext";
 
 const Navbar = () => {
+	const { dispatch } = useContext(DarkModeContext);
 	return (
 		<div className="navbar">
 			<div className="wrapper">
@@ -23,7 +25,10 @@ const Navbar = () => {
 						English
 					</div>
 					<div className="item">
-						<DarkModeOutlinedIcon className="icon" />
+						<DarkModeOutlinedIcon
+							className="icon"
+							onClick={() => dispatch({ type: "TOGGLE" })}
+						/>
 					</div>
 					<div className="item">
 						<FullscreenExitOutlinedIcon className="icon" />
