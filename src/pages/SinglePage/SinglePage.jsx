1416700b47577c7285801421c659a8b6d4110d8c
SolomonOjigbo <solomonojigbo@gmail.com>
2022-10-14 01:55:25 +0100

    Added the Singlepage component to display the user and product details

--- src/pages/SinglePage/SinglePage.jsx
+++ src/pages/SinglePage/SinglePage.jsx
@@ -1,8 +1,57 @@
 import React from "react";
+import Chart from "../../components/Chart/Chart";
+import Navbar from "../../components/Navbar/Navbar";
+import Sidebar from "../../components/Sidebar/Sidebar";
+import Table from "../../components/Table/Table";
 import "./SinglePage.scss";
+import User1 from "../../assests/user1.jpg";
 
 const SinglePage = () => {
-	return <div>SinglePage</div>;
+	return (
+		<div className="single">
+			<Sidebar />
+			<div className="singleContainer">
+				<Navbar />
+				<div className="top">
+					<div className="left">
+						<div className="editButton">Edit</div>
+						<h1 className="title">Information</h1>
+						<div className="item">
+							<img src={User1} alt="" className="itemImg" />
+							<div className="details">
+								<h1 className="itemTitle">Jane Doe</h1>
+								<div className="detailItem">
+									<span className="itemKey">Email:</span>
+									<span className="itemValue">janedoe@gmail.com</span>
+								</div>
+								<div className="detailItem">
+									<span className="itemKey">Phone:</span>
+									<span className="itemValue">+1 2345 67 89</span>
+								</div>
+								<div className="detailItem">
+									<span className="itemKey">Address:</span>
+									<span className="itemValue">
+										Elton St. 234 Garden Yd. NewYork
+									</span>
+								</div>
+								<div className="detailItem">
+									<span className="itemKey">Country:</span>
+									<span className="itemValue">USA</span>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div className="right">
+						<Chart aspect={3 / 1} title="User Spending ( Last 6 Months)" />
+					</div>
+				</div>
+				<div className="bottom">
+					<h1 className="title">Last Transactions</h1>
+					<Table />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default SinglePage;
